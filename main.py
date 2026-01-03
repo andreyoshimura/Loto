988fb0fdb566f222b2bd33bf138a7999220a158b
Andre Yoshimura <48725940+andreyoshimura@users.noreply.github.com>
2026-01-03 19:29:09 -0300

    Update main.py
    comentários

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Script para gerar automaticamente uma imagem JPG a partir de um arquivo de fundo (PNG ou JPG),
+inserindo textos centralizados com a data atual e um título fixo relacionado à Lotofácil.
+O resultado final é salvo como 'lotofacil.jpg' para uso em publicações ou automações diárias.
+Imagem de entrada : fundo.png
+Imagem de saida : lotofacil.jpg
+Local: Diretorio Raiz
+"""
+
 import datetime
 from PIL import Image, ImageDraw, ImageFont
 

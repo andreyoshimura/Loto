f71dc2ca0aa0bd07d366d1feba5159dd3d6bf5fa
Andre Yoshimura <48725940+andreyoshimura@users.noreply.github.com>
2026-01-23 23:37:53 -0300

    Update gerar_imagem.py

--- gerar_imagem.py
+++ gerar_imagem.py
@@ -4,9 +4,18 @@ import shutil
 from PIL import Image, ImageDraw, ImageFont
 
 CAMPANHAS = {
-    1:  "fundos/janeiro_branco.png",
-    9:  "fundos/setembro_amarelo.png",
-    10: "fundos/outubro_rosa.png",
+    1:  "fundos/janeiro_branco.png",        # Janeiro Branco
+    2:  "fundos/fevereiro_roxo.png",        # (ex.: Lúpus/Alzheimer etc. se você usar)
+    3:  "fundos/marco_azul_marinho.png",    # (ex.)
+    4:  "fundos/abril_verde.png",           # (ex.)
+    5:  "fundos/maio_amarelo.png",          # (ex.)
+    6:  "fundos/junho_vermelho.png",        # (ex.)
+    7:  "fundos/julho_amarelo.png",         # (ex.)
+    8:  "fundos/agosto_dourado.png",        # (ex.)
+    9:  "fundos/setembro_amarelo.png",      # Setembro Amarelo
+    10: "fundos/outubro_rosa.png",          # Outubro Rosa
+    11: "fundos/novembro_azul.png",         # Novembro Azul
+    12: "fundos/dezembro_vermelho.png", 
 }
 
 def atualizar_fundo():
